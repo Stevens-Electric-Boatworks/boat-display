5afe97e094ea6068193de2167860d1a1c3bb7012
Thiago Andrade <tja.usa.202@gmail.com>
2025-11-28 15:05:32 -0500

    Add initital support for React Router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { RosProvider } from "./components/ros-provider.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <RosProvider>
-    <App />
-  </RosProvider>
+  <BrowserRouter>
+    <RosProvider>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+      <App />
+    </RosProvider>
+  </BrowserRouter>
 );
